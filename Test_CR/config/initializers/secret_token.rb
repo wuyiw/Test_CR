7ad13f6b4d3ab386be0d386d0423ef543c5bb99f
Wu Yi <aa966@126.com>
2011-07-21 17:35:57 +0800

    t3

---
+++ Test_CR/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestCR::Application.config.secret_token = 'fad0344b72310680a80694769cbe259775e61542391c317565b035366b6325626ff7115cc2be1b447d759848b5e91000b0b2bccd13b7a92d8811fe0ae10318e8'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestCR::Application.config.secret_token = '5caed8c69ceb5c16fc4c0958e83d54ddabcaca1cd03bc418719397e68fc1f86bd638b434fc3a43d7f823f7ea4b96f72e2b34ddd47071cbe31cd99f086b1706ff'
